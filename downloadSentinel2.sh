cabaf44e0268d3481c86c92736e5e24a9be53250
ryfeus <ryfeus@gmail.com>
2020-11-18 10:04:38 -0800

    Add download script

---
+++ downloadSentinel2.sh
@@ -0,0 +1,3 @@
+aws s3 cp s3://sentinel-s2-l1c/tiles/37/R/DP/2020/10/31/0/B08.jp2 B08.jp2 - request-payer requester
+aws s3 cp s3://sentinel-s2-l1c/tiles/37/R/DP/2020/10/31/0/B04.jp2 B04.jp2 - request-payer requester
+aws s3 cp s3://sentinel-s2-l1c/tiles/37/R/DP/2020/10/31/0/B03.jp2 B03.jp2 - request-payer requester
